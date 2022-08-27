34cd60810222d77e99af16bd10854b79b5a83e71
jordaneldridge <Jordan@codedifferently.com>
2022-08-27 11:17:29 -0400

    Help Desk Form

--- src/main/java/jordan/eldridge/helpdesksever/domain/helpRequest/model/HelpRequest.java
+++ src/main/java/jordan/eldridge/helpdesksever/domain/helpRequest/model/HelpRequest.java
@@ -22,6 +22,6 @@ public class HelpRequest {
     private String firstName;
     private String lastName;
     private String email;
-    private String description;
+    //private String description;
 
 }
